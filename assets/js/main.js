352a8f165442ba60e37b36fe2ba307f3dae3fddc
jcbrognez <jcbrognez@gmail.com>
2020-08-11 12:59:26 +0200

    clean

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,204 +9,104 @@ let words = [
     "csharp",
     "algorithm",
     "compiler",
+    "css",
 ];
 
-let maxGuesses = 7; // max tries = how many failed guesses
+let answer = '';
+let maxWrong = 6;
+let mistakes = 0;
+let guessed = [];
+let wordStatus = null;
 
-let guessedLetters = []; // store array unique letters guessed
-let word = ""; // index to store randomly selected word in array
-let guessingWord = []; // array that stores guessed letters
-let remainingGuesses = 7; // tries left
-let hasFinished = false; // for 'press any key to try again'
-let wins = 0; // how many wins
-
-
-    word = words[Math.floor(Math.random() * words.length)];
-    let array = word.split('');
-    console.log(array);
-
-    let tableau = [];
-    tableau = array.map(element => element = "_");
-    console.log(tableau);
-    document.getElementById('currentWord').innerHTML= tableau.join('');
-
-
-    /*
-let letter = addEventListener(onkeydown)...
-function compare() {
-    //loop array
-    // if array[i] = letter;
-    // tableau[i] = letter;
+function randomWord() {
+    answer = words[Math.floor(Math.random() * words.length)];
 }
-*/
+
+randomWord();
+
+function generateButtons() {
+    let buttonHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
+        `
+        <button
+        class="btn btn-md btn-primary m-1"
+        id='` + letter + `'
+        onclick="handleGuess('` + letter + `')"
+        >
+        ` + letter + `
+        </button>
+        `);
+
+        document.getElementById('keyboard').innerHTML = buttonHTML.join('');
+}
+
+generateButtons();
+
+document.getElementById('maxWrong').innerHTML = maxWrong;
+
+// replace underscore by guessed letter if exist, updates the letters
+function guesseWord() {
+    wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+
+    document.getElementById('wordSpotLight').innerHTML = wordStatus;
+}
+
+guesseWord();
 
 
-// reset game-level variables
-function resetGame() {
-    remainingGuesses = maxGuesses; // reset number of remaining tries
+function handleGuess(chosenLetter) {
+    //if chosen letter does'nt exist push in array, if does exist, do nothing
+    guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null; // null : disable
+    document.getElementById(chosenLetter).setAttribute('disabled', true); // disable the buttons when used
 
-    // pick a random word from array
-    let word = words[Math.floor(Math.random() * words.length)];
-    let array = word.split(" ");
-    console.log(array);
-
-    // clear out arrays
-    guessedLetters = [];
-    guessingWord = [];
-
-    // clear hangman-image
-    document.getElementById('hangman-image').src = "";
-
-    // build guessing word and clear it out
-    for (let i = 0; i < word.length; i++) {
-        guessingWord.push("_"); // initialize word with '_' to be replaced with correct answers
+    if (answer.indexOf(chosenLetter) >= 0) { // does exist
+        guesseWord(); // updates the letters
+        checkWin();
+    } else if (answer.indexOf(chosenLetter) === -1) { // does not exist
+        mistakes++; //increment mistakes
+        updateMistakes(); // and update
+        checkLoss();
+        updateImage(); // update picture
     }
-    
-    // hide game over and win text/image
-    document.getElementById("pressKeyTryAgain").style.cssText = "display: none";
-    document.getElementById("gameover-image").style.cssText = "display: none";
-    document.getElementById("win-image").style.cssText = "display: none";
+}
 
-    // show display
-    updateDisplay();
-};
-
-
-// update the display on HTML page
-function updateDisplay() {
-    document.getElementById("totalWins").innerText = wins;
-
-    let guessingWordText = "";
-
-    for (let i = 0; i < guessingWord.length; i ++) {
-        guessingWordText += guessingWord[i];
+// update picture
+function updateImage() {
+    if (mistakes < 6) {
+        document.getElementById('hangman-image').src = 'assets/img/' + mistakes + '.jpg';
+    } else if (mistakes === 6) {
+        document.getElementById('hangman-image').src = 'https://media.giphy.com/media/VCD63s5JpEgs8/giphy.gif';
     }
+}
 
-    document.getElementById('currentWord').innerHTML = guessingWordText;
-    document.getElementById('remainingGuesses').innertHTML = remainingGuesses;
-    document.getElementById('guessedLetters').innerHTML = guessedLetters;
-};
+//UPDATE mistakes
+function updateMistakes() {
+    document.getElementById('mistakes').innerHTML = mistakes;
+}
 
-// update hanging image depending on number of guesses
-function updateHangmanImage() {
-    document.getElementById('hangman-image').src = "assets/img/" + (maxGuesses - remainingGuesses) + ".jpg";
-};
-
-// event listener on key
-document.addEventListener("onkeydown", function(event){
-    if (hasFinished) {
-        resetGame();
-        hasFinished = false; // allow to restart the game
-    } else {
-        // check if a-z pressed
-        if (event.keycode >= 65 && event.keyCode <= 90) {
-            makeGuess(event.key.toLowerCase()); // force lowercase input
-            updateDisplay();
-            checkWin();
-            checkLoss();
-        }
-    }
-});
-
-// make guesses function
-function makeGuess(letter) {
-    if (remainingGuesses > 0) { // check if remaining guesses
-
-        /* REPLACE BY LOOP */
-        // verify if already used letter, search in guessedLetters Array
-        if (guessedLetters.indexOf(letter) === -1) { // if retuns -1 -> not in array -> push
-            guessedLetters.push(letter);
-            evaluateGuess(letter); // pass letter to evaluateGuess
-        }
-    }
-
-};
-
-
-// search in word for all instances in the string and replace in guess word
-function evaluateGuess(letter) {
-    // array to store positions of letters in string
-    let positions = [];
-
-    // loop through word
-    for (let i = 0; i < word.length; i++) {
-        if (word[i] === letter) { // find instances
-            positions.push(i); // store in array
-        }
-    }
-
-    // if no indicies
-    if (positions.length <= 0) {
-        remainingGuesses --; // decrement guesses
-        updateHangmanImage(); // update hanging image
-    } else  {
-        // loop through indicies and replace '_' with letter
-        for (let i = 0; i < positions.length; i ++) {
-            guessingWord[positions[i]] = letter;
-        }
-    }
-};
-
-// checking for a win
+// WIN
 function checkWin() {
-    if (guessingWord.indexOf("_") === 1) {
-        document.getElementById("win-image").style.cssText = "displa: block";
-        document.getElementById("pressKeyTryAgain").style.cssText = "displa: block";
-        wins ++;
-        hasFinished = true;
+    if (wordStatus === answer) {
+        document.getElementById('hangman-image').src = 
+        document.getElementById('keyboard').innerHTML = 'You won !';
+        document.getElementById('hangman-image').src = 'https://media.giphy.com/media/aBktINOpjrWAE/giphy.gif';
     }
-};
- // chacking for a loss
+}
+
+// LOSS
 function checkLoss() {
-    if (remainingGuesses <= 0) {
-        document.getElementById('gameover-image').style.cssText = 'display: block';
-        document.getElementById('pressKeyTryAgain').style.cssText = 'display: block';
-        hasFinished = true;
+    if (mistakes === maxWrong) {
+        document.getElementById('keyboard').innerHTML = 'You dig !';
+        document.getElementById('wordSpotLight').innerHTML = 'The answer was : ' + answer;
     }
 }
 
+function reset() {
+    mistakes = 0;
+    guessed = [];
+    document.getElementById('hangman-image').src = 'assets/img/0.jpg';
 
-/*
-// answer array
-let answerArray = [];
-// create looping variable 
-for (let i = 0; i < word.length; i ++) {
-    answerArray[i] = "_";
+    randomWord();
+    guesseWord();
+    updateMistakes();
+    generateButtons();
 }
-// variable to keep tracks of remaing letters and decrement guesses
-let remainingLetters = word.length;
-
-// game loop
-while (remaingLetters > 0) {
-    // show progress
-    document.getElementById('answer').innerHTML = answerArray.join(" ");
-    // take input
-    let guess = prompt("Guess a letter");
-    if (guess === null) {
-        break; // exit the game loop
-    }   else if (guess.length !== 1) {
-        alert("Please enter a single letter");
-    } else {
-        //update game state with the guess
-        for (let j = 0; j < word.length; j ++) {
-            // update answerArray and remainingLetters if good guess
-            if (word[j] === guess) {
-                answerArray[j] = guess;
-                remainingLetters --;
-            }
-        }
-    }
-    // end of loop
-}
-
-// show final result
-document.getElementById('answer').innerHTML = answerArray.join(" ");
-alert("Good job ! The answer was " + word);
-
-// add variable to track number of guesses and end game if runs out
-// and in while (remaing > 0 && nbGues > 0)
-
-// FIX BUG (if guess same correct letter, remainingLetters keeps decrementing)
-// add other condition to check if value in answerArray is still underscore
-// if not underscore, then letter has already been guessed
-*/
